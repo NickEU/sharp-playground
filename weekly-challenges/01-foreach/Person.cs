a58a08a2878878853085e9269381fd097a09f632
Nikolajs Korolkovs <k@nick.tf>
2020-09-12 08:19:03 +0300

    weekly: +01-forEach

---
+++ weekly-challenges/01-foreach/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+    }
+}
---
+++ weekly-challenges/01-foreach/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var people = new List<Person> { new Person("Amy", "Winehouse"),
+                new Person("John", "Crichton"),
+                new Person("Ralph", "Kramden"),
+                new Person("Artie", "Lange"),
+                new Person("Kana", "Rua")
+            };
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"Hello {person.FirstName} {person.LastName}!");
+            }
+        }
+    }
+}
